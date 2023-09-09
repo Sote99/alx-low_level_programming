f2deab0bf1b91d8c99bb3897f424e72e449f0123
Sote99 <Soteolayinka2@gmail.com>
2023-09-09 08:00:53 -0700

    C world 2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: 0
- */
-int main(void)
-{
-	int ones = '0';
-	int tens = '0';
 
-	for (ones = '0'; ones <= '9'; ones++)
-		for (tens = '0'; tens <= '9'; tens++)
-			if (!((ones == tens) || (tens > ones))
-					putchar(ones);
-					putchar(tens);
-					if (!(ones == '9' && tens == '8'))
-					putchar(',');
-					putchar(' ');
-					putchar('\n');
-
-					return (0);
+int main() {
+    int ones, tens; // '0'; '0'; 
+    
+    for (tens = '0'; tens <= '9'; tens++) {
+        for (ones = '0'; ones <= '9'; ones++) {
+            if (tens < ones) {
+                putchar(tens);
+                putchar(ones);
+                if (tens != '8' || ones != '9') {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    
+    putchar('\n');
+    
+    return 0;
 }
+
