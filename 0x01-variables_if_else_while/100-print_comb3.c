fa647e6584cf13168aa9d580aa4ba83431547685
Sote99 <Soteolayinka2@gmail.com>
2023-09-09 08:20:26 -0700

    C world 2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 
-int main() {
-    int ones, tens; // '0'; '0'; 
-    
-    for (tens = '0'; tens <= '9'; tens++) {
-        for (ones = '0'; ones <= '9'; ones++) {
-            if (tens < ones) {
-                putchar(tens);
-                putchar(ones);
-                if (tens != '8' || ones != '9') {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    
-    putchar('\n');
-    
-    return 0;
-}
 
+/**
+ * main -prints all possible different combinations of two digits
+ * in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+			continue;
+
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
