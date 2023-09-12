6442f8d94ab1e9a27f746e9420a688622ef0f156
Sote99 <Soteolayinka2@gmail.com>
2023-09-12 06:04:53 -0700

    C world 3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print alphabet - function of the task
  * Return: 0
  */
 
 void print_alphabet(void);
 {
+
 	char m;
 
-	for (m = 'a' ; m <= 'z' ; m++)
+	for (m = 'a'; m <= 'z'; m++)
 		_putchar(m);
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
