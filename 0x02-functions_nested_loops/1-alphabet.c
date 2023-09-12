90ce11228067189ac5ec1c4c169ccf382ea7565c
Sote99 <Soteolayinka2@gmail.com>
2023-09-12 05:19:00 -0700

    C world

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+void print_alphabet(void);
+{
+	char m;
+
+	for (m = 'a' ; m <= 'z' ; m++)
+		_putchar(m);
+	putchar('\n');
+	return (0);
+}
