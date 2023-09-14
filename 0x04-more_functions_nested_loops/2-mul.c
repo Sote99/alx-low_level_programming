d4e4a1a84077b8ce9ee5934658667dc16016ca6d
Sote99 <Soteolayinka2@gmail.com>
2023-09-14 14:26:12 -0700

    C world 5

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - mul 2 int
+ * @f: first int
+ * @g: second int
+ * Return: addition of the multiplicattion
+ */
+
+int mul(int f, int g)
+{
+	return (f * g);
+}
+
