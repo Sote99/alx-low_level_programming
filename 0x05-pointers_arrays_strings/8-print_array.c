ca7c851d19fc809be0aa83e73178cc6807717641
Sote99 <Soteolayinka2@gmail.com>
2023-09-18 15:43:15 -0700

    C world

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,16 +2,28 @@
 /**
  * print_array - Function
  * @a: parameter 1
- * @n: parameter 2
+ * @n: parameter
+ * Return: 0
  */
 
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
-		printf("%d", a[i]);
-	if (i < n - 1)
-		printf(",");
+	for (i = 0; i < (n - 1); i++)
+		printf("%d,", a[i]);
+
+	if (i == (n - 1))
+		printf("%d", a[n - 1]);
 	printf("\n");
 }
+
+
+
+
+
+
+
+
+
+
