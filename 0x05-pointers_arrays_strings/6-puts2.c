bf9a80e342c4dc80eec33f17e0c13c6fd2ed35bf
Sote99 <Soteolayinka2@gmail.com>
2023-09-18 11:10:07 -0700

    C world

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 -Function
+ * @str: Parameter
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	_putchar('\n');
+}
