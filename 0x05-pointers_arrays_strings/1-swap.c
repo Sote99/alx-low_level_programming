ccdfb76faf04395b05bdbc71a9aa2215ad9666a1
Sote99 <Soteolayinka2@gmail.com>
2023-09-18 06:38:20 -0700

    C world

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * swap_integer -Function name
+ * @a: first parameter
+ * @b: second parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+	int z;
+
+	z = *a;
+	*a = *b;
+	*b = z;
+}
+
+
+
+
+
+
+
+
+
+
