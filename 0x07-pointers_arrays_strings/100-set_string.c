6060912354c36c204612b59f9c15a8e56c810d8c
Sote99 <Soteolayinka2@gmail.com>
2023-09-25 05:25:44 -0700

    C world

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * set_string - function
+  *
+  * @s: parameter 1
+  *
+  * @to: parameter 2
+  *
+  * Return: 0
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
