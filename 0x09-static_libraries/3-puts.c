380f38ae236cd068f1eb9f69957895e61ef94130
Sote99 <Soteolayinka2@gmail.com>
2023-10-02 09:01:32 -0700

    C world

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - Entry point
- * Return: 0
+ * _puts - Function
+ * @str: parameter
  */
-int main(void)
+
+void _puts(char *str)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar (str[i]);
+	_putchar ('\n');
 }
